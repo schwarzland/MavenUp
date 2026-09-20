3b34eb045e43054715dc206ba4cd68cfd97ddad3
michael <mischwar@t-online.de>
2026-09-20 22:05:38 +0200

    Remove unused constants and redundant test setup from `DependencyHierarchyPanel` and related tests

--- src/main/kotlin/de/schwarzland/mavenup/ui/DependencyHierarchyPanel.kt
+++ src/main/kotlin/de/schwarzland/mavenup/ui/DependencyHierarchyPanel.kt
@@ -38,7 +38,6 @@ import java.awt.Color
 import java.awt.event.KeyEvent
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
-import javax.swing.JComponent
 import javax.swing.JTree
 import javax.swing.KeyStroke
 import javax.swing.tree.DefaultMutableTreeNode
